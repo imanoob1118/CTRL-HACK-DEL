97ddb34c0211975bccabd02cc0323fe0e2ca36b8
Ethan <110430934+etthann@users.noreply.github.com>
2024-11-09 15:50:04 -0500

    Created Main Screen and Navbar

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,26 @@
+import { Navbar, Nav, Container, Col, Button } from "react-bootstrap";
+import * as Icon from 'react-bootstrap-icons';
+import '../../pages/Home/Home.css';
+
+
+export default function NavBar() {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary justify-content-between">
+            <Container fluid>
+                <Navbar.Brand href="/" className="mx-5">SignLanguageAI</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Col xs='auto'>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="/home" className="mx-3">Home</Nav.Link>
+                            <Nav.Link href="/about" className="mx-3">About</Nav.Link>
+                            <Nav.Link href="/features" className="mx-3">Features</Nav.Link>
+                            <Nav.Link href="/pricing" className="mx-3">Pricing</Nav.Link>
+                            <Button variant="success" type="submit" className="ml-auto mx-3 custom-button">Begin Now <Icon.ArrowRight /></Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Col>
+            </Container>
+        </Navbar>
+    );
+}
